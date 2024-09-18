23789be311fde8e379ae88d1819c0d78a5013635
Алсу Нугаева <nugaeva2003a@mail.ru>
2024-09-18 22:57:40 +0300

    Reapply "add dbcontext and CreateDatabase migration"
    This reverts commit 30722e0609a0e1e0a32f713154e4539d40f73761.

---
+++ Nugaeva Alsu OZKT-42-21/Database/Models/Group.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Nugaeva_Alsu_OZKT_42_21.Database.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+
+        public string GroupName { get; set; }
+
+
+    }
+}
---
+++ Nugaeva Alsu OZKT-42-21/Database/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace Nugaeva_Alsu_OZKT_42_21.Database.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public int GroupId { get; set; }
+
+        public Group Group { get; set; }
+    }
+}
