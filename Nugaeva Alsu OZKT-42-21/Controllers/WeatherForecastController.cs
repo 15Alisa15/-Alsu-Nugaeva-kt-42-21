30722e0609a0e1e0a32f713154e4539d40f73761
Алсу Нугаева <nugaeva2003a@mail.ru>
2024-09-18 22:52:00 +0300

    Revert "add dbcontext and CreateDatabase migration"
    This reverts commit 3ed2bf0f0f001ea27dcd0c7c9020b598e4e4bce2.

--- Nugaeva Alsu OZKT-42-21/Controllers/WeatherForecastController.cs
+++ Nugaeva Alsu OZKT-42-21/Controllers/WeatherForecastController.cs
@@ -7,46 +7,33 @@ using System.Threading.Tasks;
 
 namespace Nugaeva_Alsu_OZKT_42_21.Controllers
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class WeatherForecastController : ControllerBase
-	{
-		private static readonly string[] Summaries = new[]
-		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-		};
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
-		private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<WeatherForecastController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
-		{
-			_logger = logger;
-		}
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
 
-		[HttpGet]
-		public IEnumerable<WeatherForecast> Get()
-		{
-			_logger.LogInformation("WeatherForecast endpoint was called");
-
-			var rng = new Random();
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = rng.Next(-20, 55),
-				Summary = Summaries[rng.Next(Summaries.Length)]
-			})
-			.ToArray();
-		}
-
-		[HttpPost(Name = "AddNewSummary")]
-		public string[] AddNewSummary(string newSummary)
-		{
-			_logger.LogError("New method was called");
-
-			var list = Summaries.ToList();
-			list.Add(newSummary);
-			return list.ToArray();
-		}
-	}
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
 }
-
--- Nugaeva Alsu OZKT-42-21/Program.cs
+++ Nugaeva Alsu OZKT-42-21/Program.cs
@@ -1,75 +1,26 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog;
-using NLog.Web;
-using Nugaeva_Alsu_OZKT_42_21.Database;
-
-
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Nugaeva_Alsu_OZKT_42_21
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-			var logger = LogManager.Setup()
-				.LoadConfigurationFromAppSettings()
-				.GetCurrentClassLogger();
-
-			try
-			{
-				builder.Logging.ClearProviders();
-				builder.Host.UseNLog();
-
-				builder.Services.AddControllers();
-				builder.Services.AddEndpointsApiExplorer();
-				builder.Services.AddSwaggerGen();
-
-				// Контекст БД
-				var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-				builder.Services.AddDbContext<NugaevaDbContext>(options =>
-					options.UseSqlServer(connectionString));
-
-				// Добавляем наши сервисы
-				AddCustomServices(builder.Services);
-
-				var app = builder.Build();
-
-				if (app.Environment.IsDevelopment())
-				{
-					app.UseSwagger();
-					app.UseSwaggerUI();
-				}
-
-				app.UseAuthorization();
-
-				app.MapControllers();
-
-				app.Run();
-			}
-			catch (Exception ex)
-			{
-				logger.Error(ex, "Stopped program because of exception");
-			}
-			finally
-			{
-				LogManager.Shutdown();
-			}
-		}
-
-		private static void AddCustomServices(IServiceCollection services)
-		{
-			// Здесь добавляются наши пользовательские сервисы
-			// Например:
-			// services.AddSingleton<IYourService, YourServiceImpl>();
-			// services.AddScoped<IScopeService, ScopeServiceImpl>();
-		}
-	}
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
